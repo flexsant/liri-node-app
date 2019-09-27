7be2122162906fa2975e4f8d9d06d0f925ee7d9c
flexsant <flexsant@gmail.com>
2019-09-26 21:18:54 -0700

    run Spotify function

--- liri.js
+++ liri.js
@@ -10,31 +10,77 @@ var spotify = new Spotify(keys.spotify);
 
 var inquirer = require("inquirer");
 
+var fs = require("fs");
+
+var action = process.argv[2];
+
+var search = process.argv.slice(3).join("");
+
+
+// concert-this
+
+
+// spotify-this-song
+
+
+// movie-this
+
+
+// do-what-it-says
 
 // Spotify inquirer prompt asking for user input
-inquirer.prompt([
+// inquirer.prompt([
 
-    {
-        type: "input",
-        name: "artist",
-        message: "Choose an artist?"
-    },
+//     {
+//         type: "input",
+//         name: "artist",
+//         message: "Choose an artist?"
+//     },
 
-    {
-        type: "list",
-        name: ""
 
-    },
-
-]).then(function (user) {
-    console.log(user);
-    spotify.search({ type: 'track', query: user.artist }, function (err, data) {
+// function for spotify
+// ])
+// .then(function (user) {
+// console.log(user);
+var runSpotify = function () {
+    spotify.search({ type: 'track', query: search, limit: 1 }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-
-        console.log(data);
+        var artist = data.tracks.items[0].artists[0].name;
+        var songName = data.tracks.items[0].name;
+        var preview = data.tracks.items[0].preview_url;
+        var album = data.tracks.items[0].album.name;
+        console.log("Artist: " , artist);
+        console.log(songName);
+        console.log(preview);
+        console.log(album);
     });
+}
 
 
-})
+// })
+
+// function for omdb
+
+// function bandsintown
+
+// function do what it says
+
+// main processes
+
+if (action === "spotify-this-song") {
+    runSpotify();
+} else if (action === "concert-this") {
+    console.log("concert-this");
+}
+else if (action === "movie-this") {
+    console.log("movie-this");
+
+}
+else if (action === "do-what-it-says") {
+    console.log("do-what-it-says");
+}
+else {
+    console.log("Liri doesn't understand");
+}
